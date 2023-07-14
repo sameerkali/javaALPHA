755beba9c46178080180529e532e4e307927c58d
Sameer Faridi <s.faridi007@gmail.com>
2023-07-14 15:06:30 +0530

    Adding Greedy

--- Greedy/coin.java
+++ Greedy/coin.java
@@ -8,7 +8,7 @@ public class coin {
     Integer[] coins = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
     Arrays.sort(coins, Comparator.reverseOrder());
 
-    int amount = 590;
+    int amount = 2002;
     int countOfCoins = 0;
     ArrayList coinsUsed = new ArrayList<>();
 
